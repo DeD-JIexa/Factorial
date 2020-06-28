588985329b99845af2e50b775cbfc1bcb9df1863
Alex <alex3kot@outlook.com>
2020-06-28 23:40:41 +0400

    Добавьте файлы проекта.

---
+++ Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число");
+            string a = Console.ReadLine();
+            int b = Convert.ToInt32(a);
+            Console.WriteLine("Факториал: " + Factorial(b));
+        }
+
+        static int Factorial(int a)
+        {
+            if (a == 0) return 1;
+            return a * Factorial(a - 1);
+        }
+    }
+}
